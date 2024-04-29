3a94881fee8d9d844dbb4619f5d5512665a17a1b
unstoppableayush <ayushkum884@gmail.com>
2024-04-29 18:26:33 +0530

    Made ecommerce management Data Modelling

---
+++ 04. Data Modelling for Backend with Mongoose/models/ecommerce/order.models.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const orderItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product"
+  },
+  quantity: {
+    type: Number,
+    required: true
+  }
+})
+
+const orderSchema = new mongoose.Schema({
+  orderPrice: {
+    type: Number,
+    required: true
+  },
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  orderItems: {
+    type: [orderItemSchema]
+  },
+  address: {
+    type: String,
+    required: true,
+    enum: ["PENDING","CANCELLED", "DELIVERED"],
+    default: "PENDING"
+  },
+
+}, {timestamps: true})
